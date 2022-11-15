369d2cde706a135d1788f215351bcd1244d6230b
danifaso02 <danifaso02@gmail.com>
2022-11-16 00:41:38 +0100

    Fine

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 "use: strict";
 
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+const shoppingList = ["Pasta", "Riso", "Fiesta","Pomodori", "Aglio" ];
+
+const List = document.getElementById("shopping-list");
+
+let currentIndex = 0;
+
+while(currentIndex <= shoppingList.length -1){
+
+    let item = document.createElement("li");
+
+    item.innerHTML = shoppingList[currentIndex];
+
+    List.append(item);
+
+    currentIndex++;
+}
